2d5d8db6a1be0d5c36d40cd0e8b12e439939bd93
Charlemagne Wong <frian13@hotmail.com>
2016-03-13 18:44:12 -0500

    TaskDescription additions and client updates from grip formula repository

--- Team 5427/src/org/usfirst/frc/team5427/robot/network/TaskDescription.java
+++ Team 5427/src/org/usfirst/frc/team5427/robot/network/TaskDescription.java
@@ -3,6 +3,6 @@ package org.usfirst.frc.team5427.robot.network;
 import java.io.Serializable;
 
 public enum TaskDescription implements Serializable {
-	GOAL_ATTACHED, LOG, MESSAGE, AUTO_START, TELEOP_START
+	GOAL_ATTACHED, LOG, MESSAGE, AUTO_START, TELEOP_START, DEFAULT_MODE
 
 }
